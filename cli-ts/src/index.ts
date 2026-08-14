233bf3692a7224b30de32ee8da769623834424f9
burakonkn <burakonkn@gmail.com>
2026-08-14 11:26:42 +0300

    feat: wire CLI entry point and add tsx-based run scripts

--- cli-ts/src/index.ts
+++ cli-ts/src/index.ts
@@ -1,2 +1,5 @@
-const message: string = "orderflow ts cli running";
-console.log(message);
+import { parseArguments } from "./cli/arguments.js";
+import { runCommand } from "./cli/commands.js";
+
+const parsed = parseArguments(process.argv.slice(2));
+await runCommand(parsed);
